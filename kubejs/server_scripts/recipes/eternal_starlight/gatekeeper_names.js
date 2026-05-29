75c32771df7836ff101c42604f0b3958002e02d2
Piotr1628 <idpiotrek@gmail.com>
2026-05-29 14:58:35 +0200

    Removing datapacks

---
+++ kubejs/server_scripts/recipes/eternal_starlight/gatekeeper_names.js
@@ -0,0 +1,6 @@
+ServerEvents.generateData('after_mods', (event) => {
+  event.json('eternal_starlight:eternal_starlight/gatekeeper_names', {
+    replace: true,
+    values: ['Mike_Endercraft', 'THEEKANTETJE', 'Blackjaxk', 'UnrealSniper84', 'paperpapre'],
+  });
+});
