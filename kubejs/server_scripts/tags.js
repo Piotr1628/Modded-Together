c4c3286d91abaf76f9eec53a5126e1fdd3b4be83
Piotr1628 <idpiotrek@gmail.com>
2026-05-29 14:40:19 +0200

    Changes to KJS

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -2,7 +2,14 @@
 ServerEvents.tags('item', (event) => {
   event.add('hazentouvelib:focus/radiance_focus', 'eternal_starlight:starlit_diamond');
   event.add('mynethersdelight:boiled_egg_candidate', '#c:eggs');
-  event.add('c:eggs', ['reanimal:kiwi_egg', 'reanimal:ostrich_egg', 'reanimal:pigeon_egg', 'reanimal:vulture_egg', 'reanimal:penguin_egg', 'reanimal:crocodile_egg']);
+  event.add('c:eggs', [
+    'reanimal:kiwi_egg',
+    'reanimal:ostrich_egg',
+    'reanimal:pigeon_egg',
+    'reanimal:vulture_egg',
+    'reanimal:penguin_egg',
+    'reanimal:crocodile_egg',
+  ]);
   event.add('nomoremagicchoices:skill_weapon', '#minecraft:enchantable/weapon');
 });
 
@@ -13,6 +20,15 @@ ServerEvents.tags('block', (event) => {
 
 // EMNTITIES
 ServerEvents.tags('entity_type', (event) => {
-  event.add('ars_nouveau:bubble_blacklist', ['create:contraption', 'create:stationary_contraption', 'create:gantry_contraption', 'create:carriage_contraption']);
+  event.add('ars_nouveau:bubble_blacklist', [
+    'create:contraption',
+    'create:stationary_contraption',
+    'create:gantry_contraption',
+    'create:carriage_contraption',
+  ]);
   event.add('iceandfire:immune_to_gorgon_stone', []);
+  event
+    .get('ars_nouveau:jar_blacklist')
+    .removeAll()
+    .add([/ars_nouveau/, /cataclysm/, /irons_spellbooks/]);
 });
